3f845dc3c751375cfbe6e425ffcc82871bdf69a9
Parakoopa <parakoopa@live.de>
2021-01-02 17:51:27 +0100

    Copyright update 2021

--- explorerscript/ssb_converting/ssb_data_types.py
+++ explorerscript/ssb_converting/ssb_data_types.py
@@ -1,7 +1,7 @@
 """Data types used to describe ssb components"""
 #  MIT License
 #
-#  Copyright (c) 2020 Parakoopa
+#  Copyright (c) 2020-2021 Parakoopa and the SkyTemple Contributors
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
